ccbe9ecc3e6e2583c32b68d902b72068aeeb6940
gyes00205 <gyes00205@gmail.com>
2021-11-24 04:58:37 +0800

    Follow PEP8 guidelines

--- detect.py
+++ detect.py
@@ -5,20 +5,19 @@ import numpy as np
 import json
 from PIL import Image
 import matplotlib.pyplot as plt
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
 import pathlib
 import tensorflow as tf
+import time
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
 tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
 
 # Enable GPU dynamic memory allocation
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-
-import time
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as viz_utils
 
 parser = argparse.ArgumentParser(description='Download and process tf files')
 parser.add_argument('--saved_model_path', required=True,
@@ -34,15 +33,17 @@ parser.add_argument('--label_map', required=True,
 args = parser.parse_args()
 PATH_TO_SAVED_MODEL = os.path.join(args.saved_model_path, "saved_model")
 PATH_TO_TEST_IMAGE = args.test_path
-PATH_TO_OUTPUT_IMAGE = args.output_path 
+PATH_TO_OUTPUT_IMAGE = args.output_path
 MIN_SCORE_THRESH = float(args.min_score_thresh)
 PATH_TO_LABELS = args.label_map
 
 os.makedirs(PATH_TO_OUTPUT_IMAGE, exist_ok=True)
 # Load the Labels
 
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
-                        use_display_name=True)
+category_index = label_map_util.create_category_index_from_labelmap(
+    PATH_TO_LABELS,
+    use_display_name=True
+)
 
 # Load saved model and build the detection function
 print('Loading model...', end='')
@@ -61,17 +62,19 @@ for filename in TOTAL_TEST_IMAGES:
     print('Running inference for {}... '.format(filename_path))
     image_np = cv2.imread(filename_path)
     image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-    
+
     input_tensor = tf.convert_to_tensor(image_np)
     # The model expects a batch of images, so add an axis with `tf.newaxis`.
     input_tensor = input_tensor[tf.newaxis, ...]
     detections = detect_fn(input_tensor)
     # All outputs are batches tensors.
-    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension
     # We're only interested in the first num_detections.
     num_detections = int(detections.pop('num_detections'))
-    detections = {key: value[0, :num_detections].numpy()
-                    for key, value in detections.items()}
+    detections = {
+        key: value[0, :num_detections].numpy()
+        for key, value in detections.items()
+    }
     detections['num_detections'] = num_detections
 
     # detection_classes should be ints.
@@ -79,20 +82,9 @@ for filename in TOTAL_TEST_IMAGES:
 
     image_np_with_detections = image_np.copy()
 
-#     viz_utils.visualize_boxes_and_labels_on_image_array(
-#           image_np_with_detections,
-#           detections['detection_boxes'],
-#           detections['detection_classes'],
-#           detections['detection_scores'],
-#           category_index,
-#           use_normalized_coordinates=True,
-#           max_boxes_to_draw=200,
-#           min_score_thresh=MIN_SCORE_THRESH,
-#           agnostic_mode=False)
+    indexes = [i for i in range(len(detections["detection_scores"]))
+               if detections["detection_scores"][i] > MIN_SCORE_THRESH]
 
-    indexes = [ i for i in range(len(detections["detection_scores"])) 
-          if detections["detection_scores"][i] > MIN_SCORE_THRESH]
-    
     detections["detection_boxes"] = detections["detection_boxes"][indexes, ...]
     detections["detection_scores"] = detections["detection_scores"][indexes, ...]
     detections["detection_classes"] = detections["detection_classes"][indexes, ...]
@@ -106,7 +98,7 @@ for filename in TOTAL_TEST_IMAGES:
         category_id = int(category_index[classes]['name'])
         w = float(xmax - xmin)
         h = float(ymax - ymin)
-        
+
         det_box_info["image_id"] = image_id
         det_box_info["bbox"] = [xmin, ymin, w, h]
         det_box_info["score"] = scores
@@ -114,7 +106,10 @@ for filename in TOTAL_TEST_IMAGES:
 
         result_to_json.append(det_box_info)
 
-    cv2.imwrite(os.path.join(PATH_TO_OUTPUT_IMAGE, filename), image_np_with_detections[:,:,::-1])
+    cv2.imwrite(
+        os.path.join(PATH_TO_OUTPUT_IMAGE, filename),
+        image_np_with_detections[:, :, ::-1]
+    )
     print('Done')
 
 
